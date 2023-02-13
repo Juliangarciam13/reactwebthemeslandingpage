b2e92d10dc67eb9790d014a379c3030909577692
StevenMR321 <124753231+StevenMR321@users.noreply.github.com>
2023-02-13 18:56:25 -0500

    delete app

--- src/slug/Aloha.js
+++ src/slug/Aloha.js
@@ -14,7 +14,7 @@ const Aloha = () => (
           <h1 className="display-2">
             <strong>
               <a href="/" className="text-danger">
-                Laravel Apps
+                Laravel 
               </a>
             </strong>
           </h1>
